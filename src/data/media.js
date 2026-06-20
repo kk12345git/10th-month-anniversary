818508ada742f334c8c0780d220a9b7562daf17c
kk12345git <kk12345git@example.com>
2026-06-20 11:59:46 +0530

    Update favorite photo to cover image

--- src/data/media.js
+++ src/data/media.js
@@ -1,3 +1,3 @@
 export const photos = Array.from({ length: 18 }, (_, i) => `/images/image${i + 1}.jpg`);
 export const videos = Array.from({ length: 4 }, (_, i) => `/videos/video${i + 1}.mp4`);
-export const favoritePhoto = "/images/image1.jpg";
+export const favoritePhoto = "/images/cover.jpg";
